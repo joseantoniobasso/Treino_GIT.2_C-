440a96b68e851b1ec90523b75d3bbd9000f34e3f
José Antonio <joseantonio@gmail.com>
2024-04-09 07:49:24 -0300

    criado txt names

--- obj/Debug/net8.0/Treino_GIT_C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/Treino_GIT_C#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Treino_GIT_C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8701f3e0b6ee08a5f0c49c4791a7ad046bedfa15")]
 [assembly: System.Reflection.AssemblyProductAttribute("Treino_GIT_C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Treino_GIT_C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
